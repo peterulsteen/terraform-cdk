b38cac3a6da5dfb759c0a1b0539bc17ccbebe78b
Ansgar Mertens <ansgar@hashicorp.com>
2021-07-06 08:57:04 +0200

    wip

--- packages/cdktf-cli/bin/cmds/ui/watch.tsx
+++ packages/cdktf-cli/bin/cmds/ui/watch.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-control-regex */
-import React, { useState, useCallback, useEffect } from "react";
-import { useTerraform, Status, useTerraformState } from "./terraform-context";
+import React, { useRef, useState, useCallback, useEffect } from "react";
+import { Status, useTerraform, useTerraformState } from "./terraform-context";
 import { Text, Box, Newline } from "ink";
 import chokidar from "chokidar";
 
@@ -18,20 +18,46 @@ export const Watch = ({
   autoApprove,
 }: WatchConfig): React.ReactElement => {
   const state = useTerraformState();
-  const { synth } = useTerraform({
+  const { synth, deploy } = useTerraform({
     targetDir,
     targetStack,
     synthCommand,
-    autoApprove,
   });
-  // TODO: use some statemachine
+  // TODO: use some statemachine to only run one synth at a time
   const queueSynth = useCallback(() => {
-    const x = synth();
-    console.log(x);
+    synth(true);
   }, [synth]);
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const queueDeployRef = useRef<{ callback: () => void }>({ callback: () => {} });
+
+  const queueDeploy = useCallback(() => {
+    if (state.status === Status.SYNTHESIZED) {
+      console.log("GOGOGO deploy()");
+      deploy();
+    } else {
+      // requeue
+      console.log(`deferred deploy as status was ${state.status}`);
+
+      setTimeout(queueDeployRef.current.callback, 500);
+    }
+  }, [deploy, state.status]);
+
+  useEffect(() => { queueDeployRef.current.callback = queueDeploy }, [queueDeploy]);
+
+  const [lastStartTime, setLastStartTime] = useState(0);
+  const [lastSynthTime, setLastSynthTime] = useState(0);
+
   useEffect(() => {
-    chokidar
+    if (state.status === Status.SYNTHESIZING) {
+      setLastStartTime(Date.now());
+    } else if (state.status === Status.SYNTHESIZED) {
+      setLastSynthTime(Date.now() - lastStartTime);
+    }
+  }, [state.status]);
+
+  useEffect(() => {
+    const watcher = chokidar
       .watch(".", {
         ignored: [
           "node_modules/**",
@@ -42,17 +68,32 @@ export const Watch = ({
           "terraform.demo-cdktf-ts-docker.tfstate",
         ],
       })
-      .on("all", (event, path) => {
-        console.log(`${event} ${path}`);
-        // addEvent(`${event} ${path}`);
-      })
       .on("change", (path) => {
         console.log(`path changed: ${path}`);
         queueSynth();
       });
+    return function cleanup() {
+      watcher.close();
+    };
   }, []);
 
-  // todo: watcher on cdktf.out dir that does diff and then deploys
+  // watch synth output and deploy
+  useEffect(() => {
+    const watcher = chokidar
+      .watch("./cdktf.out", {
+        ignored: ["**/.terraform", "**/.terraform.lock.hcl", "*/*/*/plan"],
+      })
+      .on("all", (evt, path) => {
+        // console.log({evt, path});
+      })
+      .on("change", (path) => {
+        console.log(`deploy: path changed: ${path}`);
+        queueDeploy();
+      });
+    return function cleanup() {
+      watcher.close();
+    };
+  }, []);
 
   return (
     <Box>
@@ -68,6 +109,8 @@ export const Watch = ({
         <Text>autoApprove: {autoApprove}</Text>
         <Newline />
         <Text>status: {state.status}</Text>
+        <Newline />
+        <Text>lastSynthTime: {lastSynthTime}</Text>
         <Newline />
       </Text>
     </Box>
